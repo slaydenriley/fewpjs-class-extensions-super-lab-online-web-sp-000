f4a8ac70d094af072bbe6ea88a2108be39135284
Riley Slayden <slaydenriley@gmail.com>
2020-08-17 16:09:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ class Tree {
   static definition() {
     return "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves.";
   };
-
-  
 };
+
+class Deciduous {
+  
+}
