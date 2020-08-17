f45d688ddfbe4ee50efc047bb2cdf57c98078dac
Riley Slayden <slaydenriley@gmail.com>
2020-08-17 16:12:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,4 +13,8 @@ class Deciduous extends Tree {
     super(species);
     this.name = name;
   };
+
+  static definition() {
+    return `${super.definition()} Deciduous trees shed their leaves annually.`;
+  };
 };
